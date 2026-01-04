4f252e5113a017ed28976e97da2d49c7435bc3a3
Andy Gaskin <andy@andygaskin.com>
2026-01-03 19:13:58 -0500

    testing nuxt setup

---
+++ app/pages/cakes/[id].vue
@@ -0,0 +1,6 @@
+<template>
+  <main style="padding: 2rem">
+    <h1>Cake {{ $route.params.id }}</h1>
+    <p>Cake detail page works 🎂</p>
+  </main>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <main style="padding: 2rem">
+    <h1>Cake Theater</h1>
+    <p>The show begins 🍰</p>
+  </main>
+</template>
